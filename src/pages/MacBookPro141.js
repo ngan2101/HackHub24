665a40ec370be507334976149f5ac6a6b7533c5e
ngan2101 <hoangngan21012000@gmail.com>
2024-01-05 17:43:11 -0800

    1/5/24

---
+++ src/pages/MacBookPro141.js
@@ -0,0 +1,303 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form } from "react-bootstrap";
+import "./MacBookPro141.css";
+
+const MacBookPro141 = () => {
+  return (
+    <div className="macbook-pro-14-1">
+      <div className="macbook-pro-14-1-child" />
+      <div className="macbook-pro-14-1-item" />
+      <b className="henry-nguyen">Henry Nguyen</b>
+      <div className="nav-bar">
+        <img
+          className="material-symbolsdashboard-rou-icon"
+          alt=""
+          src="/materialsymbolsdashboardrounded@2x.png"
+        />
+        <b className="dashboard">Dashboard</b>
+        <b className="task-lists">Task lists</b>
+        <img className="felist-task-icon" alt="" src="/felisttask@2x.png" />
+        <b className="inbox">Inbox</b>
+        <b className="settings">Settings</b>
+        <img className="icround-email-icon" alt="" src="/icroundemail@2x.png" />
+        <img className="nav-bar-child" alt="" src="/line-1@2x.png" />
+        <img
+          className="solarsettings-bold-icon"
+          alt=""
+          src="/solarsettingsbold@2x.png"
+        />
+      </div>
+      <img
+        className="ionnotifcations-icon"
+        alt=""
+        src="/ionnotifcations@2x.png"
+      />
+      <img className="icround-search-icon" alt="" src="/icroundsearch@2x.png" />
+      <div className="hello-henry">Hello, Henry!</div>
+      <img
+        className="ionchatbubble-outline-icon"
+        alt=""
+        src="/ionchatbubbleoutline@2x.png"
+      />
+      <img className="iconlybold2-user" alt="" src="/iconlybold2user@2x.png" />
+      <b className="hi-im-fi-container">
+        <p className="hi-im-fi">{`Hi I’m Fi, `}</p>
+        <p className="hi-im-fi">your AI assistant.</p>
+        <p className="hi-im-fi">Ask me anything!</p>
+      </b>
+      <div className="friday">Friday</div>
+      <div className="to-do">TO DO</div>
+      <div className="task-1">
+        <div className="task-1-child" />
+        <Form.Group className="task-summary-lorem-container">
+          <Form.Label>
+            Task summary: 
+          </Form.Label>
+          <Form.Control as="textarea" defaultValue="" />
+        </Form.Group>
+        <div className="div">01/08/24</div>
+        <div className="finalize-feature-2">{`Finalize Feature 2 Design & Hand-off`}</div>
+        <div className="task-1-item" />
+        <div className="task-1-inner" />
+        <div className="rectangle-div" />
+        <div className="q1-projects">Q1 Projects</div>
+        <div className="high">High</div>
+        <div className="image-4-wrapper">
+          <img className="image-4-icon" alt="" src="/image-4@2x.png" />
+        </div>
+      </div>
+      <div className="task-2">
+        <div className="task-1-child" />
+        <Form.Group className="task-summary-lorem-container1">
+          <Form.Label>
+            Task summary: 
+          </Form.Label>
+          <Form.Control as="textarea" defaultValue="" />
+        </Form.Group>
+        <div className="div1">01/09/24</div>
+        <div className="schedule-11-with">Schedule 1:1 with Intern Nina</div>
+        <div className="task-2-item" />
+        <div className="task-2-inner" />
+        <div className="team">Team</div>
+        <div className="image-3-wrapper">
+          <img className="image-3-icon" alt="" src="/image-3@2x.png" />
+        </div>
+        <div className="task-2-child1" />
+        <div className="low">High</div>
+      </div>
+      <div className="task-3">
+        <div className="task-1-child" />
+        <Form.Group className="task-summary-lorem-container2">
+          <Form.Label>
+            Task summary: 
+          </Form.Label>
+          <Form.Control as="textarea" defaultValue="" />
+        </Form.Group>
+        <div className="div1">01/09/24</div>
+        <div className="schedule-11-with">
+          Schedule coffee chat with Alex P.
+        </div>
+        <div className="task-3-item" />
+        <div className="task-2-inner" />
+        <div className="networking">Networking</div>
+        <div className="image-5-wrapper">
+          <img className="image-5-icon" alt="" src="/image-5@2x.png" />
+        </div>
+        <div className="task-3-child1" />
+        <div className="low">Low</div>
+      </div>
+      <img className="uiwreload-icon" alt="" src="/uiwreload@2x.png" />
+      <div className="update-task">Update task</div>
+      <b className="b">3</b>
+      <div className="macbook-pro-14-1-inner" />
+      <div className="day-of-the-week-parent">
+        <div className="day-of-the-week">
+          <div className="mon-wrapper">
+            <b className="mon">MON</b>
+          </div>
+          <div className="mon-wrapper">
+            <b className="mon">TUE</b>
+          </div>
+          <div className="mon-wrapper">
+            <b className="mon">WED</b>
+          </div>
+          <div className="mon-wrapper">
+            <b className="mon">THU</b>
+          </div>
+          <div className="mon-wrapper">
+            <b className="mon">FRI</b>
+          </div>
+          <div className="mon-wrapper">
+            <b className="mon">SAT</b>
+          </div>
+          <div className="mon-wrapper">
+            <b className="mon">SUN</b>
+          </div>
+        </div>
+        <div className="day-of-the-week">
+          <div className="wrapper">
+            <div className="mon">1</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">2</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">3</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">4</div>
+          </div>
+          <div className="wrapper1">
+            <div className="mon">5</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">6</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">7</div>
+          </div>
+        </div>
+        <div className="day-of-the-week">
+          <div className="wrapper">
+            <b className="mon">8</b>
+          </div>
+          <div className="wrapper">
+            <b className="mon">9</b>
+          </div>
+          <div className="wrapper">
+            <div className="mon">10</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">11</div>
+          </div>
+          <div className="wrapper">
+            <b className="mon">12</b>
+          </div>
+          <div className="wrapper">
+            <div className="mon">13</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">14</div>
+          </div>
+        </div>
+        <div className="day-of-the-week">
+          <div className="wrapper">
+            <div className="mon">15</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">16</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">17</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">18</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">19</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">20</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">21</div>
+          </div>
+        </div>
+        <div className="day-of-the-week">
+          <div className="wrapper">
+            <div className="mon">22</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">23</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">24</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">25</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">26</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">27</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">28</div>
+          </div>
+        </div>
+        <div className="day-of-the-week">
+          <div className="wrapper">
+            <div className="mon">28</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">30</div>
+          </div>
+          <div className="wrapper">
+            <div className="mon">31</div>
+          </div>
+          <div className="week-5-child" />
+          <div className="week-5-child" />
+          <div className="week-5-child" />
+          <div className="week-5-child" />
+        </div>
+      </div>
+      <div className="january-05-2024">JANUARY 05, 2024</div>
+      <div className="macbook-pro-14-1-child1" />
+      <div className="inbox-overview">INBOX OVERVIEW</div>
+      <div className="image-1-wrapper">
+        <img className="image-1-icon" alt="" src="/image-1@2x.png" />
+      </div>
+      <div className="macbook-pro-14-1-child2" />
+      <div className="on-going-threads">
+        <p className="hi-im-fi">On-going</p>
+        <p className="hi-im-fi">threads</p>
+      </div>
+      <div className="div31">8</div>
+      <div className="macbook-pro-14-1-child3" />
+      <div className="unread-emails">
+        <p className="hi-im-fi">Unread</p>
+        <p className="hi-im-fi">emails</p>
+      </div>
+      <div className="div32">5</div>
+      <div className="macbook-pro-14-1-child4" />
+      <div className="not-yet-replied-container">
+        <p className="hi-im-fi">Not yet</p>
+        <p className="hi-im-fi">replied</p>
+      </div>
+      <div className="div33">15</div>
+      <div className="view-all-tasks-wrapper">
+        <b className="mon">View all tasks</b>
+      </div>
+      <div className="view-full-inbox-wrapper">
+        <b className="mon">View full inbox</b>
+      </div>
+      <div className="macbook-pro-14-1-child5" />
+      <div className="macbook-pro-14-1-child6" />
+      <img className="vector-icon" alt="" src="/vector@2x.png" />
+      <div className="macbook-pro-14-1-child7" />
+      <div className="image-8-wrapper">
+        <img className="image-8-icon" alt="" src="/image-8@2x.png" />
+      </div>
+      <b className="quang-pham">Quang Pham</b>
+      <img className="vector-icon1" alt="" src="/vector@2x.png" />
+      <div className="re-ux-design">{`Re: UX Design Org Change `}</div>
+      <div className="image-6-wrapper">
+        <img className="image-6-icon" alt="" src="/image-6@2x.png" />
+      </div>
+      <b className="lucas-smith">Lucas Smith</b>
+      <div className="re-feature-1">{`Re: Feature 1 Research Readout   `}</div>
+      <div className="image-7-wrapper">
+        <img className="image-7-icon" alt="" src="/image-7@2x.png" />
+      </div>
+      <b className="linh-duong">Linh Duong</b>
+      <div className="re-time-off-request">{`Re: Time-off Request  `}</div>
+      <img className="vector-icon2" alt="" src="/vector@2x.png" />
+      <div className="ellipse-div" />
+      <div className="macbook-pro-14-1-child8" />
+      <div className="macbook-pro-14-1-child9" />
+    </div>
+  );
+};
+
+export default MacBookPro141;
